c528ebe70098a43ab89b79d0a11d9117bcf3e9ad
Anderson Lugo <andersonlugojacome@hotmail.com>
2025-11-11 10:45:22 -0500

    Bump app version to 1.1.7 (versionCode 7)
    Updated versionCode from 4 to 7 and versionName from 1.1.0 to 1.1.7 in build.gradle.kts to prepare for a new release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.digitalesweb.zapaticocochinito"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.1.0"
+        versionCode = 7
+        versionName = "1.1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
